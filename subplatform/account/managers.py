b6bf1ab249897738fe8e7aaf945ed5a8f69ab7ad
Liberbell <liberbell@gmail.com>
2025-12-09 08:48:11 +0900

    managers3

--- subplatform/account/managers.py
+++ subplatform/account/managers.py
@@ -9,3 +9,6 @@ class CustomUserManager(BaseUserManager):
         if not email:
             raise ValueError(_("The email must be set"))
         email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save

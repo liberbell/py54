b4c9b83f60a27a4c6586e8b566747b8b4a5a7ec0
Liberbell <liberbell@gmail.com>
2025-12-10 08:28:14 +0900

    models3

--- subplatform/account/models.py
+++ subplatform/account/models.py
@@ -8,3 +8,7 @@ class CustomUser(AbstractUser, PermissionsMixin):
     username = None
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=80)
+    last_name = models.CharField(max_length=135)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(_(""), auto_now=False, auto_now_add=False)

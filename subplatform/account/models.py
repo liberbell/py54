cf63395a8982a9da854e56f8521b8cac6e8cc840
Liberbell <liberbell@gmail.com>
2025-12-10 08:51:46 +0900

    models8

--- subplatform/account/models.py
+++ subplatform/account/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from .managers import CustomUserManager
+from . managers import CustomUserManager
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.utils import timezone
 

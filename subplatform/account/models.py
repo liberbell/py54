65a11ffa99d4494799d729d460b2919d07274271
Liberbell <liberbell@gmail.com>
2025-12-10 08:26:26 +0900

    models2

--- subplatform/account/models.py
+++ subplatform/account/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 from .managers import CustomUserManager
 from django.contrib.auth.models import AbstractUser, PermissionsMixin
+from django.utils import timezone
 
 # Create your models here.
+class CustomUser(AbstractUser, PermissionsMixin):
+    username = None
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=80)

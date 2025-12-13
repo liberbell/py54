b7c57003bf2f058e91932e7d78b3d618d12271aa
Liberbell <liberbell@gmail.com>
2025-12-13 10:57:00 +0900

    views4

--- subplatform/account/views.py
+++ subplatform/account/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from . forms import CreateUserForm
+from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
@@ -11,6 +12,9 @@ def register(request):
     form = CreateUserForm()
     if request.method == "POST":
         form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("User registered.")
     
     return render(request, "account/register.html")
 

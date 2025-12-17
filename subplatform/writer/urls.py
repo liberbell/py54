206706725c41a4164e311576344d3f6a5322d1b3
Liberbell <liberbell@gmail.com>
2025-12-18 07:45:53 +0900

    urls3

--- subplatform/writer/urls.py
+++ subplatform/writer/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('client-dashboard', views.writer_dashboard, name="writer-dashboard"),
+    path('writer-dashboard', views.writer_dashboard, name="writer-dashboard"),
 ]

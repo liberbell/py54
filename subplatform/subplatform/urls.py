3ba4d85fada1c8fa3f740c5555ff837057506d5f
Liberbell <liberbell@gmail.com>
2025-12-18 07:16:07 +0900

    urls1

--- subplatform/subplatform/urls.py
+++ subplatform/subplatform/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('account.urls')),
     path('client/', include('client.urls')),
+    path('writer/', include('writer.urls')),
 ]

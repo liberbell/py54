8c49e8c8c83554d5da94676fc549ea4ce1261b40
Liberbell <liberbell@gmail.com>
2025-12-19 09:24:07 +0900

    views1

--- subplatform/client/views.py
+++ subplatform/client/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-@login_required("my-login")
+@login_required(login_url="my-login")
 def client_dashboard(request):
 
     return render(request, "client/client-dashboard.html")

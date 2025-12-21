c9004f83be6c4da6722f37755ba938cd4c87656f
Liberbell <liberbell@gmail.com>
2025-12-22 08:16:41 +0900

    urls1

--- subplatform/writer/urls.py
+++ subplatform/writer/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('writer-dashboard', views.writer_dashboard, name="writer-dashboard"),
+    path('create-article', views.create_article, name="create-article"),
 ]

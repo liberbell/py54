0dd22bf3ce0a8f2e96b19e7a2860f5ddb677e715
Liberbell <liberbell@gmail.com>
2025-12-21 14:16:05 +0900

    models2

--- subplatform/writer/models.py
+++ subplatform/writer/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from account.models import CustomUser
 
 # Create your models here.
 class Article(models.Model):
@@ -6,3 +7,7 @@ class Article(models.Model):
     title = models.CharField(max_length=150)
     content = models.TextField(max_length=1000)
     date_posted = models.DateTimeField(auto_now_add=True)
+
+    is_premium = models.BooleanField(default=False, verbose_name="Is this a premium article?")
+
+    user = models.ForeignKey(CustomUser, max_length=10, on_delete=models.CASCADE)

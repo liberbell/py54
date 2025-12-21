075d75892aedb7b5bd1535f311906bc121b1bae2
Liberbell <liberbell@gmail.com>
2025-12-21 14:11:27 +0900

    models1

--- subplatform/writer/models.py
+++ subplatform/writer/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+
+    title = models.CharField(max_length=150)
+    content = models.TextField(max_length=1000)
+    date_posted = models.DateTimeField(auto_now_add=True)

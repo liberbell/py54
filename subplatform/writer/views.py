c3ae82b1eb1c8bc1443a35e565ea6f3d28d634e6
Liberbell <liberbell@gmail.com>
2025-12-26 08:28:53 +0900

    views1

--- subplatform/writer/views.py
+++ subplatform/writer/views.py
@@ -41,7 +41,11 @@ def my_articles(request):
 @login_required(login_url="my-login")
 def update_article(request, pk):
 
-    article = Article.objects.get(id=pk)
+    try:
+        article = Article.objects.get(id=pk, user=request.user)
+    except:
+        return redirect("my-articles")
+
     form = ArticleForm(instance=article)
 
     if request.method == "POST":

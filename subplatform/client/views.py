54ec50a7e544829fb3962bee9acd0a5457ed343c
Liberbell <liberbell@gmail.com>
2025-12-30 17:57:00 +0900

    views5

--- subplatform/client/views.py
+++ subplatform/client/views.py
@@ -16,3 +16,8 @@ def browse_articles(request):
         subDetails = Subscription.objects.get(user=request.user, is_active=True)
     except:
         return redirect("client-dashboard")
+    
+    current_subscription_plan = subDetails.subscription_plan
+
+    if current_subscription_plan == "Standard":
+        articles = Article.objects.all().filter(is_premium=False)

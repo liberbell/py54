9e43482950e029d553b01d3a9e2c492a5949a3d7
Liberbell <liberbell@gmail.com>
2026-01-01 14:55:41 +0900

    urls1

--- subplatform/client/urls.py
+++ subplatform/client/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('client-dashboard', views.client_dashboard, name="client-dashboard"),
     path('browse-articles', views.browse_articles, name="browse-articles"),
     path('subscription-locked', views.subscription_locked, name="subscription-locked"),
+    path('subscription-plans', views.subscription_plans, name="subscription-plans"),
 ]

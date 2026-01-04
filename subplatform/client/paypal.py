4bc289cb56b700d10adfde3b9aa19e9b9d7f18fc
Liberbell <liberbell@gmail.com>
2026-01-05 08:49:11 +0900

    paypal4

--- subplatform/client/paypal.py
+++ subplatform/client/paypal.py
@@ -7,3 +7,4 @@ def get_access_token():
     data = {'grant_type': 'client-credentials'}
     headers = {'Accesp' : 'application/json', 'Accept-Language': 'en_US'}
     client_id = "AfWI9P33RrvMnsP0OeerAriRlYOYMkGiHcN6z40KW7mDy1VF66OUUx5tNY9jsumECNbXhb74EEz0svAp"
+    secret_id = "something"

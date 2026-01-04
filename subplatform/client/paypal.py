24677fc0fc56107267fa2978ab1db3b34baec651
Liberbell <liberbell@gmail.com>
2026-01-05 08:47:59 +0900

    paypal3

--- subplatform/client/paypal.py
+++ subplatform/client/paypal.py
@@ -5,3 +5,5 @@ from . models import Subscription
 def get_access_token():
 
     data = {'grant_type': 'client-credentials'}
+    headers = {'Accesp' : 'application/json', 'Accept-Language': 'en_US'}
+    client_id = "AfWI9P33RrvMnsP0OeerAriRlYOYMkGiHcN6z40KW7mDy1VF66OUUx5tNY9jsumECNbXhb74EEz0svAp"

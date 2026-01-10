74c56d88f18aef81e8e4924c0d7602cac94b567d
Liberbell <liberbell@gmail.com>
2026-01-10 11:10:12 +0900

    urls1

--- subplatform/client/urls.py
+++ subplatform/client/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('create-subscription/<subID>/<plan>', views.create_subscription, name="create-subscription"),
     path('delete-subscription/<subID>', views.delete_subscription, name="delete-subscription"),
     path('update-subscription/<subID>', views.update_subscription, name="update-subscription"),
+    path('paypal-update-sub-confirmed', views.paypal_update_sub_confirmed, name="paypal-update-sub-confirmed"),
 ]

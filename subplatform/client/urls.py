6e2023077de5afa27c69826c7b5ceb36626c9696
Liberbell <liberbell@gmail.com>
2026-01-11 11:41:58 +0900

    urls2

--- subplatform/client/urls.py
+++ subplatform/client/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('delete-subscription/<subID>', views.delete_subscription, name="delete-subscription"),
     path('update-subscription/<subID>', views.update_subscription, name="update-subscription"),
     path('paypal-update-sub-confirmed', views.paypal_update_sub_confirmed, name="paypal-update-sub-confirmed"),
-    path('django-update-sub-confirmed', views.django_update_sub_confirmed, name="django-update-sub-confirmed"),
+    path('django-update-sub-confirmed/<subID>', views.django_update_sub_confirmed, name="django-update-sub-confirmed"),
 ]

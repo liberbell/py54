bb5cb403d6b1819b1a7763b18818644b3ffd1476
Liberbell <liberbell@gmail.com>
2026-01-17 10:44:32 +0900

    urls5

--- subplatform/account/urls.py
+++ subplatform/account/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('reset_password', auth_views.PasswordResetView.as_view(), name="reset_password"),
     path('reset_passwor_sent', auth_views.PasswordResetDoneView.as_view(), name="reset_password_done"),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path('password_reset_complete', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 ]

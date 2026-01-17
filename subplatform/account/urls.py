2bf8a696574a91a6e5bc5611430a899f902fc6f1
Liberbell <liberbell@gmail.com>
2026-01-17 10:24:39 +0900

    urls1

--- subplatform/account/urls.py
+++ subplatform/account/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name=""),
     path('register', views.register, name="register"),
     path('my-login', views.my_login, name="my-login"),
     path('user-logout', views.user_logout, name="user-logout"),
+    path('reset_password', auth_views.PasswordResetView(), name="reset_password"),
 ]

25460e806c89cd77e484a8b790cab4c7d8f0db5e
Liberbell <liberbell@gmail.com>
2026-01-18 18:50:15 +0900

    urls2

--- subplatform/account/urls.py
+++ subplatform/account/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('my-login', views.my_login, name="my-login"),
     path('user-logout', views.user_logout, name="user-logout"),
     path('reset_password', auth_views.PasswordResetView.as_view(template_name="account/password-reset.html"), name="reset_password"),
-    path('reset_passwor_sent', auth_views.PasswordResetDoneView.as_view(), name="reset_password_done"),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path('reset_passwor_sent', auth_views.PasswordResetDoneView.as_view(template_name="account/password-reset-sent.html"), name="reset_password_done"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="account/password-reset-form.html"), name="password_reset_confirm"),
     path('password_reset_complete', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 ]

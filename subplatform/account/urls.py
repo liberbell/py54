98c7a03d4cb0cf96573d4647bad4d4791ac7074d
Liberbell <liberbell@gmail.com>
2026-01-24 10:16:29 +0900

    urls1

--- subplatform/account/urls.py
+++ subplatform/account/urls.py
@@ -14,5 +14,5 @@ urlpatterns = [
     path('email-verification', views.email_verification, name="email-verification"),
     path('email-verification-sent', views.email_verification_sent, name="email-verification-sent"),
     path('email-verification-success', views.email_verification_success, name="email-verification-success"),
-    path('email-verification-failed', views.email_verification_failed, name="email-verification-faailed"),
+    path('email-verification-failed', views.email_verification_failed, name="email-verification-failed"),
 ]
